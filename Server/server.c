503a1c70fc8798247d76155e1c4a7856f33f699c
arjun-csekar <32626731+arjun-csekar@users.noreply.github.com>
2018-02-26 19:53:33 -0800

    Add files via upload

--- Server/server.c
+++ Server/server.c
@@ -6,7 +6,8 @@
 
 int main(){
 	
-	char *msg = "This is server";
+	char msg[256] = "This is server";
+	char msg1[256];
 	
 	int server_socket, client_socket;
 	
@@ -19,13 +20,17 @@ int main(){
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
 	bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-	
+
 	listen(server_socket, 3);
-	
 	client_socket = accept(server_socket, NULL, NULL);
-	
+	while(client_socket){
 	send(client_socket, msg, sizeof(msg), 0);
 	
+	
+	recv(client_socket, &msg1,  sizeof(msg), 0);
+	
+	printf("%s", msg1);
+	}
 	close(server_socket);
 	return 0;
 }
